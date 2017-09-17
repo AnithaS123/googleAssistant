e53cac3b3df61ed48b9e7ce4b40614a433f87688
38136 <38136.hexaware@gmail.com>
2017-09-17 06:50:21 +0530

    Map issue

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(bodyParser.json({
 
 // get by action
 
+
+let startdate = "2017-09-13";
+
+
+
 const TrackByFlight_ID = "TrackByFlightID";
 const TrackByStarting_Date = "TrackByStartingDate";
 const Help_Intent = "HelpIntent";
@@ -89,20 +94,23 @@ app.post('/', function (req, res) {
                     let departureDate = res.body.flightTrack.departureDate.dateLocal;
                     let airName = res.body.appendix.airlines[0].name;
                     let airPortName = res.body.appendix.airports[0].name;
-                    let airPortCity = res.body.appendix.airports[0].city;
-                    let airPortCountryName = res.body.appendix.airports[0].countryName;
+                    let airPortLat = res.body.appendix.airports[0].latitude;
+                    let airPortLong = res.body.appendix.airports[0].longitude;
+                    var airPortCity = res.body.appendix.airports[0].city;
+                    var airPortCountryName = res.body.appendix.airports[0].countryName;
                     let airPortregionName = res.body.appendix.airports[0].regionName;
-                    let airPortlat = res.body.appendix.airports[0].latitude;
-                    let airPortlong = res.body.appendix.airports[0].longitude;
-
+                    var airPortlat = res.body.appendix.airports[0].latitude;
+                    var airPortlong = res.body.appendix.airports[0].longitude;
                     var deptdate = new Date(departureDate);
-
                     console.log("logging flight id " + flightId);
+                    // https: //maps.googleapis.com/maps/api/staticmap?center=45.588995,-122.592901&zoom=14&size=400x400&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&key=AIzaSyBdMRmNmPYEkXlEjFe30tIGzAVOwxMdij4
+                   
 
-                    FlightTrackdata = `Your flight Id is ${flightId} the maximum positions is ${maxPositions} and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is ${departureDate} and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong}. Do you want to continue. `;
-                    assistant.ask(FlightTrackdata);
-                    //  response.say(JSON.stringify(res));
-                    response.send();
+                    assistant.ask(assistant.buildBasicCard()
+                        .addSimpleResponse(`Your flight Id is ${flightId} the maximum positions is ${maxPositions} and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is ${departureDate} and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong}. Do you want to continue.`)
+                        .setTitle('Flight tracked in')
+                        .setImage(`https: //maps.googleapis.com/maps/api/staticmap?center=${airPortLat},${airPortLong}&zoom=14&size=400x400&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&key=AIzaSyBdMRmNmPYEkXlEjFe30tIGzAVOwxMdij4`,'Map not loaded')
+                    );
                 });
             return p;
         } else {
@@ -116,34 +124,49 @@ app.post('/', function (req, res) {
         var flightNumber = assistant.getArgument('flightNumber');
         console.log("date is been displayed" + startDate);
         console.log("the response is " + response);
-        var year = Date(startDate).year;
 
-        var month = Date(startDate).month;
-        var day = Date(startDate).day;
-        console.log("this is year" + year);
-        console.log("this is month" + month);
-        console.log("this is day" + day);
+
+
+        var date = new Date(startDate);
+
+        var year = date.getFullYear();
+        var current_month = date.getMonth() + 1;
+        var month = (current_month < 10 ? "0" : "") + current_month;
+        var current_day = date.getDate();
+        var day = (current_day < 10 ? "0" : "") + current_day;
+
+        console.log("the hours is :" + year);
+        console.log("the hours is :" + month);
+        console.log("the hours is :" + day);
+        console.log("the response is " + AirLineCode);
+        console.log("the response is " + flightNumber);
+
         if (flightNumber) {
             if (AirLineCode) {
                 if (startDate) {
-                    var p = Promise.resolve();
+                    var k = Promise.resolve();
                     var getDetails_date = {
                         method: 'GET',
                         // 933427129 flight number
                         // https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/tracks/AA/100/arr/2017/09/13?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&utc=false&includeFlightPlan=false&maxPositions=2
                         // uri: `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/track/${flightNumber_url}?appId=${apiId}&appKey=${apiKey}&includeFlightPlan=false&maxPositions=2`,
-                        uri: `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/tracks/${AirLineCode}/${flightNumber}/arr/${year}/${month}/${date}?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&utc=false&includeFlightPlan=false&maxPositions=2`,
+                        uri: `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/tracks/${AirLineCode}/${flightNumber}/arr/${year}/${month}/${day}?appId=6aac18a6&appKey=40a7e359cb020a07ead5159c2d5d8162&utc=false&includeFlightPlan=false&maxPositions=2`,
                         json: true,
                         resolveWithFullResponse: true,
                     };
-                    console.log("get details log " + getDetails_date);
-                    p = rp(getDetails_date)
+                    console.log("get details log " + JSON.stringify(getDetails_date));
+                    k = rp(getDetails_date)
                         .then(function (res) {
+
+                            console.log("this is res inside the function" + JSON.stringify(res))
+
                             let flightId = res.body.request.airline.requestedCode;
                             let maxPositions = res.body.request.maxPositions.requested;
-                            let fLNumber = res.body.flightTrack[0].flightNumber;
-                            let carrierCode = res.body.flightTrack[0].carrierFsCode;
-                            let departureDate = res.body.flightTrack[0].departureDate.dateLocal;
+                            console.log("logging flight id " + flightId);
+                            console.log("logging maxPositions " + maxPositions);
+                            let fLNumber = res.body.flightTracks[0].flightNumber;
+                            let carrierCode = res.body.flightTracks[0].carrierFsCode;
+                            let departureDate = res.body.flightTracks[0].departureDate.dateLocal;
                             let airName = res.body.appendix.airlines[0].name;
 
                             let airPortName = res.body.appendix.airports[0].name;
@@ -153,22 +176,22 @@ app.post('/', function (req, res) {
                             let airPortlat = res.body.appendix.airports[0].latitude;
                             let airPortlong = res.body.appendix.airports[0].longitude;
 
-                            console.log("logging flight id " + flightId);
 
-                            FlightTrackByDatedata = `Your flight Id is ${flightId}  the maximum positions is ${maxPositions}  and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is today and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong} `;
+
+                            FlightTrackByDatedata = `Your flight Id is ${flightId}  the maximum positions is ${maxPositions}  and flight number is ${fLNumber} the carrier code is  ${carrierCode} and the departure date is today and the airport name is ${airPortName} and the airport city name is ${airPortCity} and the country name is ${airPortCountryName} the lattitude are ${airPortlat} logitude is ${airPortlong}. Do you want to continue. `;
                             assistant.ask(FlightTrackByDatedata);
                             //  response.say(JSON.stringify(res));
                             response.send();
                         });
-                    return p;
+                    return k;
                 } else {
-                    assistant.ask("please give me your Arrival date");
+                    assistant.ask("Give me your Arrival date");
                 }
             } else {
                 assistant.ask("please give me your Air line code example AA");
             }
         } else {
-            assistant.ask("please tell me your Flight Number example 100");
+            assistant.ask("Give me your Flight Number. example 100");
         }
     }
 
